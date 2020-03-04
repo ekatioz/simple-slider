482a51b8f8739ec448894d2570ed3a106111d5e6
Marco Ernst <marco.ernst@thalia.de>
2020-03-04 16:02:14 +0100

    add Keyboad and Arrow support

--- src/main.js
+++ src/main.js
@@ -1,5 +1,51 @@
-document.querySelectorAll(".item").forEach(item =>
-  item.addEventListener("click", () => {
-    item.scrollIntoView({ behavior: "smooth", inline: "center" });
+const items = Array.from(document.querySelectorAll(".item")).map(item => ({
+  container: item,
+  radio: item.querySelector("input[type='radio']")
+}));
+
+const controls = {
+  next : document.querySelector(".control-button.icon-next"),
+  prev : document.querySelector(".control-button.icon-prev")
+}
+
+items.forEach(({ container, radio }) =>
+  radio.addEventListener("change", e => {
+    scrollToCenter(container);
   })
 );
+
+addEventListener("keydown", event => {
+  if (event.key === "ArrowRight") {
+    selectNext();
+    event.preventDefault();
+  } else if (event.key === "ArrowLeft") {
+    selectPrev();
+    event.preventDefault();
+  }
+});
+
+controls.next.addEventListener("click", () => selectNext());
+controls.prev.addEventListener("click", () => selectPrev());
+
+function scrollToCenter(element) {
+  element.scrollIntoView({ behavior: "smooth", inline: "center" });
+}
+
+function selectPrev() {
+  const active = items.indexOf(getActiveItem());
+  const prev = active == 0 ? items.length - 1 : active - 1;
+  items[prev].radio.checked = true;
+  scrollToCenter(items[prev].container);
+}
+
+function selectNext() {
+  const active = items.indexOf(getActiveItem());
+  const next = active + 1 == items.length ? 0 : active + 1;
+  items[next].radio.checked = true;
+  scrollToCenter(items[next].container);
+}
+
+function getActiveItem() {
+  const active = items.find(i => i.radio.checked);
+  return active;
+}
